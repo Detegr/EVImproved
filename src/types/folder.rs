d43577702379377fad13488114dd7fecda5b4bf1
Antti Keränen <detegr@gmail.com>
2015-01-01 22:39:54 +0200

    Started implementing Iterator trait for Folder

--- src/types/folder.rs
+++ src/types/folder.rs
@@ -36,13 +36,60 @@ pub struct FolderInfo {
 
 #[allow(dead_code)]
 pub struct Folder {
+    info: FolderInfo,
     folders: Vec<FolderInfo>,
     recordings: Vec<RecordingInfo>
+}
+
+pub struct FolderIter<'a> {
+    index: uint,
+    folder: &'a Folder
+}
+pub struct RecordingIter<'a> {
+    index: uint,
+    folder: &'a Folder
+}
+
+impl<'a> Folder {
+    fn get_id(&self) -> int {
+        self.info.id
+    }
+    fn folder_iter(&'a self) -> FolderIter<'a> {
+        FolderIter { index: 0, folder: self }
+    }
+    fn rec_iter(&'a self) -> RecordingIter<'a> {
+        RecordingIter { index: 0, folder: self }
+    }
+}
+
+impl<'a> Iterator<Folder> for FolderIter<'a> {
+    fn next(&mut self) -> Option<Folder> {
+        assert!(self.index < self.folder.folders.len());
+        let fi = &self.folder.folders[self.index];
+        self.index += 1;
+        // TODO: Fetch folder using fi
+        None
+    }
+}
+impl<'a> Iterator<Recording> for RecordingIter<'a> {
+    fn next(&mut self) -> Option<Recording> {
+        assert!(self.index < self.folder.folders.len() + self.folder.recordings.len());
+        None
+    }
 }
 
 impl<E, D : Decoder<E>> Folder {
     fn decode_folder(d: &mut D) -> Result<Folder, E> {
         Ok(Folder {
+            info: FolderInfo { // TODO
+                id: 0,
+                name: "".to_string(),
+                size: "".to_string(),
+                has_unwatched: "".to_string(),
+                has_wildcards: "".to_string(),
+                has_pin: "".to_string(),
+                recordings_count: 0
+            },
             folders: json_field!("folders", d),
             recordings: json_field!("recordings", d),
         })
@@ -65,6 +112,15 @@ impl<E, D : Decoder<E>> Decodable<D, E> for Folder {
 
 #[test]
 fn able_to_parse_readydata() -> () {
-    setup_test!("testdata/readydata.json", |_ : Folder| {
+    setup_test!("testdata/readydata.json", |_ : Folder| {});
+}
+
+#[test]
+fn able_to_iterate_folders() -> () {
+    setup_test!("testdata/readydata.json", |f : Folder| {
+        assert!(f.folder_iter().size_hint() == (0, Some(1)));
+        for fldr in f.folder_iter() {
+            assert!(fldr.get_id() == 10000001);
+        }
     });
 }
